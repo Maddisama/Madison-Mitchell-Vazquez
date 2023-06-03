1600f44cb0c50020008ada97fe975462a8a342b6
Madison <madisoncolemit@gmail.com>
2023-06-03 05:20:36 -0400

    deploy fix

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,7 +1,6 @@
 import "./index.scss";
 import LogoM from "../../assets/images/site-m.png";
 import { useEffect, useRef } from "react";
-import { DrawSVGPlugin } from "gsap-trial/DrawSVGPlugin";
 import gsap from "gsap-trial";
 
 const Logo = () => {
@@ -9,50 +8,36 @@ const Logo = () => {
   const outlineLogoRef = useRef();
   const solidLogoRef = useRef();
 
-  useEffect(() => {
-    gsap.registerPlugin(DrawSVGPlugin);
+  // useEffect(() => {
+  //   gsap.registerPlugin(DrawSVGPlugin);
 
-    gsap
-      .timeline()
-      .to(bgRef.current, {
-        duration: 1,
-        opacity: 1,
-      })
+  //   gsap
+  //     .timeline()
+  //     .to(bgRef.current, {
+  //       duration: 1,
+  //       opacity: 1,
+  //     })
 
-      .from(outlineLogoRef.current, {
-        drawSVG: 0,
-        duration: 5,
-      });
-    gsap.fromTo(
-      solidLogoRef.current,
-      {
-        opacity: 0,
-      },
-      {
-        opacity: 1,
-        delay: 4,
-        duration: 4,
-      }
-    );
-  }, []);
+  //     .from(outlineLogoRef.current, {
+  //       drawSVG: 0,
+  //       duration: 5,
+  //     });
+  //   gsap.fromTo(
+  //     solidLogoRef.current,
+  //     {
+  //       opacity: 0,
+  //     },
+  //     {
+  //       opacity: 1,
+  //       delay: 4,
+  //       duration: 4,
+  //     }
+  //   );
+  // }, []);
 
   return (
-    <div className="logo-container" ref={bgRef}>
-      <img ref={solidLogoRef} src={LogoM} alt="M" className="solid-logo" />
-      <svg
-        className="svg-container"
-        id="eJLXAqkWYGD1"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1692 2339"
-        shape-rendering="geometricPrecision"
-        text-rendering="geometricPrecision"
-      >
-        <path
-          ref={outlineLogoRef}
-          d="M96.863512,2339L86.27501,0h508.248076L885.70688,1477.095971L1187.479175,0h504.520824l-.000001,2339h-345.693301L1335.718195,746.489362L1002.180395,2339h-238.241285L440.989811,746.489362L425.107059,2339h-328.243547Z"
-          fill="none"
-        />
-      </svg>
+    <div className="logo-container">
+      <img src={LogoM} alt="M" className="solid-logo" />
     </div>
   );
 };
